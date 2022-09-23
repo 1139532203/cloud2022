955d8b20cf522807356be218fb784e159b72a014
liugualin <1139532203@qq.com>
2022-09-23 12:47:45 +0800

    second commit

--- eureka-server-7001/src/main/java/com/lgl/cloud/EurekaServerApplication7001.java
+++ eureka-server-7001/src/main/java/com/lgl/cloud/EurekaServerApplication7001.java
@@ -1,9 +1,15 @@
 package com.lgl.cloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author lgl
  * @date 2022/9/23-09-23
  */
+@SpringBootApplication
 public class EurekaServerApplication7001 {
-
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication7001.class,args);
+    }
 }
